bf61a4cc320b35b602824b97b31eafc8839dace1
USER <felixyulianasterino>
2023-11-22 14:58:20 +0700

    custom component

---
+++ android/app/src/main/kotlin/com/felix/bangun_datar_felix_c/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.felix.bangun_datar_felix_c
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/page/home_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // centerTitle: true,
+        title: Text(
+          "Home Page",
+          style: TextStyle(color: Color(0xFFFFFFFF)),
+        ),
+        backgroundColor: Colors.green,
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(child: CustomMenu(title: "Persegi Panjang", imageAsset: "assets/persegi_panjang.jpg")),
+              Expanded(child: CustomMenu(title: "Persegi", imageAsset: "assets/persegi.png")),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(child: CustomMenu(title: "Lingkaran", imageAsset: "assets/lingkaran.png")),
+              Expanded(child: CustomMenu(title: "Segitiga", imageAsset: "assets/segitiga.jpg")),
+            ],
+          ),
+
+        ],
+      ),
+    );
+  }
+}
+
+class CustomMenu extends StatelessWidget {
+  const CustomMenu({
+    super.key, required this.imageAsset, required this.title,
+  });
+
+  final String imageAsset;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        // width: 200,
+        height: 350,
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+        margin: EdgeInsets.symmetric(horizontal: 23, vertical: 16),
+        decoration: BoxDecoration(color:  Colors.green, borderRadius: BorderRadius.circular(15),),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(imageAsset, width: 150),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  title,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+    );
+  }
+}
